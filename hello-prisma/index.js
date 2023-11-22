a0f7b0672dc092880c3b1ee98a4a232d4f79f0d2
kytaec <vladislavtokovenko4@gmail.com>
2023-11-22 13:32:32 +0200

    post_prod_patch_employee

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -1,70 +1,74 @@
-const express = require('express')
+import express from 'express';
 const app = express()
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 app.use(express.json())
 
+app.get('/products', async (req, res) => {
+    const products = await prisma.products.findMany()
+    res.json(products);
+})
+
+app.post('/products', async (req, res) => {
+    const {name, category, amount, price} = req.body;
+    if (!req.body.category)
+        return res.status(403).send('Invalid product category')
+    const newProduct = await prisma.products.create({
+        data: {
+            name,
+            category,
+            amount,
+            price,
+        },
+    });
+
+    res.json(newProduct);
+})
+
+app.post('/employees', async (req, res) => {
+    const {firstName, lastName, middleName, position} = req.body;
+    if (!req.body.position)
+        return res.status(403).send('Invalid employee position')
+    const newEmployee = await prisma.employees.create({
+        data: {
+            firstName,
+            lastName,
+            middleName,
+            position,
+        },
+    });
+
+    res.json(newEmployee);
+})
+
+app.patch('/employees/:id', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
+    const {firstName, lastName, middleName, position} = req.body;
+
+    if(!await prisma.employees.findUnique({where: {id: employeeId}}))
+        return res.status(404).send('Employee with such id not found')
+
+    const employee = await prisma.employees.update({
+        where : {id: employeeId},
+        data: {
+            firstName,
+            lastName,
+            middleName,
+            position,
+        },
+    });
+
+    res.json(employee)
+
+})
+
+
+
+
+
 const port = process.env.PORT || 3000
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'}
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-})
-
-app.get('/api/courses/:id', (req, res) =>{
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send('The course with the given ID was not found')
-    res.send(course)
-})
-
-app.post('/api/courses/', (req, res) => {
-    if (!req.body.name || req.body.name.length < 3)
-        return res.status(400).send('Name is required and should be minimum 3 characters')
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course)
-    res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send('The course with the given ID was not found')
-
-    if (!req.body.name || req.body.name.length < 3)
-        return res.status(400).send('Name is required and should be minimum 3 characters')
-
-    course.name = req.body.name;
-    res.send(course);
-})
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course)
-        return res.status(404).send('The course with the given ID was not found')
-
-    const index = courses.indexOf(course)
-    courses.splice(index, 1)
-
-    res.send(course)
-})
-
-
-
-
-
-
 
 app.listen(port,() => console.log(`it's alive on http:localhost:${port}`)
 )
